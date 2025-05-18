9823ca57a019c320dc2b762d041eb85e98e418e6
Siddaramaiah <j.siddaramaiah@accenture.com>
2025-05-18 18:22:00 -0500

    add QA deployment

--- src/main/java/com/example/microservice/controller/OrderController.java
+++ src/main/java/com/example/microservice/controller/OrderController.java
@@ -1,7 +1,11 @@
 package com.example.microservice.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.microservice.service.OrderService;
-import org.springframework.web.bind.annotation.*;
 
 /* Jags created  
  * 
